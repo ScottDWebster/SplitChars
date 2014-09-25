cb3f33571693eb59e745971554de94323e4bd57d
Scott Webster <ScottW@ChefCentral.com>
2014-09-25 10:13:10 -0400

    Created splitchars.pl to turn a text file into a csv with each character being a separate piece of data.

---
+++ splitchars.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: splitchars.pl
+#
+#        USAGE: ./splitchars.pl  
+#
+#  DESCRIPTION: Splits a file into a csv with each charater in a separate cell
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Scott Webster (sdw), ScottW@ChefCentral.com
+# ORGANIZATION: Chef Central, Inc.
+#      VERSION: 1.0
+#      CREATED: 9/24/2014 4:34:04 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+my($line);
+
+while(<>)
+{
+	print join(',', split('',$_));
+}
